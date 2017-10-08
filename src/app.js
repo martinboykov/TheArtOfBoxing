d4daa087cefa6abdee91917097c1fb99b2deb0f1
Martin Martinov <martinboykov@mail.bg>
2017-10-08 09:37:18 +0300

    main section added

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import './styles/main.scss';
 import 'bootstrap';
 import './assets/favicon/favicon.ico';
+import './assets/images/border-dashed.png';
 import config from '../src/core/data/firebase';
 
 import $ from 'jquery';
