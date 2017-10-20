1db0ab33dca4e95519b2341653abc40dd7a4c120
Martin Martinov <martinboykov@mail.bg>
2017-10-20 21:44:18 +0300

    bootstrap scss added in main

--- src/build/base.config.js
+++ src/build/base.config.js
@@ -3,26 +3,18 @@ const isProd = process.env.NODE_ENV === 'production';
 const processCss = isProd ? '!postcss-loader' : ''; // autoprefixer only in ENV === 'production'
 
 
-const {
-    resolve
-} = require('path');
-
-const {
-    ProvidePlugin
-} = require('webpack');
-
+const { resolve } = require('path');
+const { ProvidePlugin } = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 const config = {
     // Include source maps in development files
     devtool: isProd ? false : '#cheap-module-source-map', // source-map only in ENV === 'development'
-
     entry: {
         // Main entry point of our app
         app: resolve(__dirname, '..', 'app.js'),
     },
-
     output: {
         // built files are stored in dist
         path: resolve(__dirname, '..', '..', 'dist'),
@@ -34,14 +26,6 @@ const config = {
         filename: '[name].[hash].js',
     },
 
-    // Automatically compile when files change.
-    // watch: true, //added with CLI --hot
-
-    //Automatically reload the page when compilation is done.
-    // devServer: {
-    //     inline: true //added with CLI --inline
-    // },
-
     resolve: {
         extensions: ['*', '.js'],
         modules: [
@@ -50,60 +34,56 @@ const config = {
         alias: {
             handlebars: 'handlebars/dist/handlebars.min.js',
         }
-
     },
 
     module: {
         rules: [{
-                test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+        },
+        {
+            test: /\.scss$/,
+            use: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: `css-loader${processCss}!resolve-url-loader!sass-loader?sourceMap`
+            }),
+        },
+        {
+            test: /\.handlebars$/,
+            loader: 'text-loader',
+        },
+        {
+            test: /\.(png|jpe?g|gif)(\?.*)?$/,
+            loader: 'url-loader',
+            options: {
+                limit: 100,
+                name: 'post/images/[name].[ext]'
             },
-            {
-                test: /\.scss$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: `css-loader${processCss}!resolve-url-loader!sass-loader?sourceMap`
-                }),
-            },
-            {
-                test: /\.handlebars$/,
-                loader: 'text-loader',
-            },
-            {
-                test: /\.(png|jpe?g|gif)(\?.*)?$/,
-                loader: 'url-loader',
-                options: {
-                    limit: 100,
-                    name: 'post/images/[name].[ext]'
-
-                },
-            },
-            {
-                test: /\.ico$/,
-                exclude: /node_modules/,
-                loader: 'file-loader',
-                options: {
-                    name: 'images/favicon/[name].[ext]'
-                }
-
-
-            },
-            {
-                test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-                loader: 'url-loader',
-                options: {
-                    limit: 100,
-                    name: 'fonts/[name].[hash:7].[ext]'
-                }
-            },
-            {
-                test: /\.(webm|mp4)$/,
-                loader: 'file-loader',
-                options: {
-                    name: 'videos/[name].[hash:7].[ext]'
-                }
-            },
+        },
+        {
+            test: /\.ico$/,
+            exclude: /node_modules/,
+            loader: 'file-loader',
+            options: {
+                name: 'images/favicon/[name].[ext]'
+            }
+        },
+        {
+            test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+            loader: 'url-loader',
+            options: {
+                limit: 100,
+                name: 'fonts/[name].[hash:7].[ext]'
+            }
+        },
+        {
+            test: /\.(webm|mp4)$/,
+            loader: 'file-loader',
+            options: {
+                name: 'videos/[name].[hash:7].[ext]'
+            }
+        },
         ],
     },
 
@@ -111,7 +91,8 @@ const config = {
         new ProvidePlugin({
             $: 'jquery',
             jQuery: 'jquery',
-            Popper: 'popper.js',
+            'window.jQuery': 'jquery',
+            Popper: ['popper.js', 'default'],
         }),
         new HtmlWebpackPlugin({
             template: resolve(__dirname, '..', 'app.html'),
